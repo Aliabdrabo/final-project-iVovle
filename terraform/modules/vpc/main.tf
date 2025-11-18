e8d9673d9dd89a7d9a98097f16edf5eca456d9dd
Aliabdrabo <alibdrabu78@gmail.com>
2025-11-18 15:12:56 +0200

    edit private-rt table

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -78,20 +78,23 @@ resource "aws_route_table_association" "public_assoc" {
 
 # Private Route Table
 resource "aws_route_table" "private_rt" {
+  count = length(var.availability_zones)
+
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = var.default_route
-    nat_gateway_id = aws_nat_gateway.nat[0].id
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
 
   tags = {
-    Name = "${var.project_name}-private-rt"
+    Name = "${var.project_name}-private-rt-${count.index + 1}"
   }
 }
 
 resource "aws_route_table_association" "private_assoc" {
   count          = length(var.availability_zones)
   subnet_id      = aws_subnet.private_subnet[count.index].id
-  route_table_id = aws_route_table.private_rt.id
+  route_table_id = aws_route_table.private_rt[count.index].id
 }
+
